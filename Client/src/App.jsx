e0e50d37054437e7336ede76b00be2a71fd49e1e
Mamta Sharma <mamta.211315@ncit.edu.np>
2024-10-28 06:33:10 +0630

    completed project

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "../src/componets/common/Navbar"
+import Home from "./componets/home/Home";
+import Registerc from "./componets/auth/customer/Registerc";
+import Role from "./componets/auth/Role";
+import Loginc from "./componets/auth/customer/Loginc";
+import ServiceRequest from "./componets/problemcreate/ServiceRequest";
+import Dashboard from "./componets/dashboard/Dashboard";
+import CustomerProfile from "./componets/customerprofile/CustomerProfile";
+import Contact from "./componets/home/component/Contact";
+import Footer from "./componets/common/Footer";
+import ProviderProfile from "./componets/dashboard/dashboardcomponent/Profile";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <Navbar/>
+      <div className="App" style={{ paddingTop:'60px' }}> 
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/register" element={<Registerc/>} /> 
+          <Route path="/login" element={<Loginc/>} /> 
+           {/* Add the Register route */}
+           <Route path="/roleselect" element={<Role/>} />
+           <Route path="/postrequest" element={<ServiceRequest/>} />
+           <Route path={'/providerdashboard/:id'} element={<Dashboard/>}/>
+
+           <Route path="/customerdashboard/:id" element={<CustomerProfile/>} />
+           <Route path='/contact' element={<Contact></Contact>}></Route>
+           <Route path='provider-profile/:id' element={<ProviderProfile/>}></Route>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    </Router>
+  );
 }
 
-export default App
+export default App;
